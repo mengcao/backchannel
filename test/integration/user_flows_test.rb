70f19096a7b43e99d05b5ae7ed8a254d6c0d66bb
meng <sirejulius@gmail.com>
2013-02-11 16:54:23 -0500

    start integration test

---
+++ test/integration/user_flows_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class UserFlowsTest < ActionDispatch::IntegrationTest
+  fixtures :all
+  # test "the truth" do
+  #   assert true
+  # end
+  test "login and logout" do
+
+  end
+end
