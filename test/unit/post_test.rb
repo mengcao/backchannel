9e9c08a68c13974a020b65723cb8bdd452b60e39
Kenny Yarboro <kbyarbor@ncsu.edu>
2013-02-11 16:31:33 -0500

    unit testing

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -4,4 +4,56 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @myPost = Post.new
+    @myPost.comments = [Comment.new, Comment.new, Comment.new, Comment.new, Comment.new]
+    @myPost.votes = [Vote.new, Vote.new, Vote.new]
+  end
+
+  test 'has many comments' do
+    assert_equal(5, @myPost.comments.size)
+  end
+
+  test 'has many votes' do
+    assert_equal(3, @myPost.votes.size)
+  end
+
+  test 'should require title and body' do
+    p = Post.new
+    assert(!(p.valid?), 'Should be false')
+  end
+
+  test 'body is accessible' do
+    p = Post.new
+    p.body = 'body' #if no error, this works
+    assert true
+  end
+
+  test 'title is accessible' do
+    p = Post.new
+    p.title = 'title' #if no error, this works
+    assert true
+  end
+
+  test 'user_id is accessible' do
+    p = Post.new
+    p.user_id = 1
+    assert true
+  end
+
+  test 'category_id is accessible' do
+    p = Post.new
+    p.category_id = 1
+    assert true
+  end
+
+  test 'can access count_vote' do
+    c = Comment.new
+    assert_respond_to(c, :count_vote, 'Should be able to access count_vote')
+  end
+
+  test 'can access voters' do
+    c = Comment.new
+    assert_respond_to(c, :voters, 'Should be able to access voters')
+  end
 end
