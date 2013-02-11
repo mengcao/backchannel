143fad193827b97300a9e635d119f0629b1a5654
Kenny Yarboro <kbyarbor@ncsu.edu>
2013-02-11 13:07:58 -0500

    admin feature

--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -4,4 +4,18 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @myCategory = [Post.new, Post.new, Post.new]
+  end
+
+  test 'name is accessible' do
+    c = Category.new
+    c.name = 'c' #if no error, this works
+    assert true
+  end
+
+  test 'has many posts' do
+    assert_equal(@myCategory.count, 3)
+  end
+
 end
