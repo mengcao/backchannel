4f0629d7276d3ec93770504ff2652bd4b9580751
meng <sirejulius@gmail.com>
2013-02-28 18:09:03 -0500

    resubmission ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -81,11 +81,17 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user = User.find(params[:id])
-    @user.destroy
+    if session[:user_id] != @user.id
+      @user.destroy
 
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
+      respond_to do |format|
+        format.html { redirect_to users_url }
+        format.json { head :no_content }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to :back,notice: 'Cannot delete current user!'}
+      end
     end
   end
 
